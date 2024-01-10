2e6d22a039cd9f507dd0004021f0202056b23d9f
GaRyung <shubom0115@naver.com>
2024-01-10 11:43:50 +0900

    #4. TodoInput 동적 렌더링

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -2,13 +2,18 @@ import React from 'react';
 import {MdDelete, MdDone} from "react-icons/md";
 import './scss/TodoItem.scss'
 
-const TodoItem = () => {
+const TodoItem = ({item}) => {
+
+    const {id, title, done} = item;
+
+    // console.log(props);
+
     return (
         <li className='todo-list-item'>
             <div className='check-circle'>
-                <MdDone />
+                {item.done && <MdDone/>} {/* props.done이 true면 */}
             </div>
-            <span className='text'>할 일 어쩌구~~</span>
+            <span className='text'>{item.title}</span>
             <div className='remove'>
                 <MdDelete />
             </div>
--- src/component/TodoMain.js
+++ src/component/TodoMain.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import './scss/TodoMain.scss'
+
+import './scss/TodoMain.scss';
 import TodoItem from "./TodoItem";
 
 const TodoMain = () => {
+
+    const todoList = [
+        {
+            id: 1,
+            title: '장보기',
+            done: false
+        },
+        {
+            id: 2,
+            title: '저녁먹기',
+            done: true
+        },
+        {
+            id: 3,
+            title: '수다떨기',
+            done: false
+        },
+        {
+            id: 4,
+            title: '축구하기',
+            done: true
+        }
+
+    ];
+
     return (
         <ul className='todo-list'>
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {
+                todoList.map(todo => <TodoItem key={todo.id} item={todo} />)
+                // map을 사용해서 할때는 key를 꼭넣어야함 수백개씩넣을때 성능이슈를 일으킴
+            }
         </ul>
     );
 };
