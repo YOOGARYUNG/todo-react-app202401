6443c2ebb0b1c955d38709aa1766ffeea05a52fb
GaRyung <shubom0115@naver.com>
2024-01-10 10:48:37 +0900

    #2. TodoTemplate 컴포넌트 분리, scss 작성

--- src/component/TodoTemplate.js
+++ src/component/TodoTemplate.js
@@ -1,39 +1,19 @@
 import { MdAdd, MdDelete, MdDone } from "react-icons/md";
 import React from "react";
 
+import './scss/TodoTemplate.scss';
+import TodoHeader from "./TodoHeader";
+import TodoInput from "./TodoInput";
+import TodoMain from "./TodoMain";
+
 const TodoTemplate = () => {
     return (
-        <>
-            <header>
-                <h1>2024년 1월 10일</h1>
-                <div class='day'>수요일</div>
-                <div class='tasks-left'>할 일 3개 남음</div>
-            </header>
+        <div className='TodoTemplate'>
+            <TodoHeader />
+            <TodoMain />
+            <TodoInput />
 
-            <ul class='todo-list'>
-                <li className='todo-list-item'>
-                    <div className='check-circle'>
-                        <MdDone />
-                    </div>
-                    <span className='text'>할 일 어쩌구~~</span>
-                    <div className='remove'>
-                        <MdDelete />
-                    </div>
-                </li>
-            </ul>
-
-            <div className='form-wrapper'>
-                <form className='insert-form'>
-                    <input
-                        type='text'
-                        placeholder='할 일을 입력 후, 엔터를 누르세요!'
-                    />
-                </form>
-            </div>
-            <button className='insert-btn'>
-                <MdAdd />
-            </button>
-        </>
+        </div>
     )
 };
 
