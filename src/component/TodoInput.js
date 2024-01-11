5e9a0fd1be4ab28f50e53139467f025408e4aace
GaRyung <shubom0115@naver.com>
2024-01-11 09:51:05 +0900

    #6. TodoInput 입력내용 TodoTemplate로 상향식 전달

--- src/component/TodoInput.js
+++ src/component/TodoInput.js
@@ -4,24 +4,41 @@ import cn from 'classnames';
 
 import './scss/TodoInput.scss'
 
-const TodoInput = () => {
+const TodoInput = ({onAdd}) => {
 
     // sueState는 렌더링 상태를 관리하는 변수지정 리액트 훅
     const [open, setOpen] = useState(false);
+
+    // 할일 입력창에 입력한 내용을 저장할 변수
+    const [todoText, setTodoText] = useState('');
 
     // 버튼 클릭 이벤트
     const onToggle = e => {
         setOpen(!open);  // 기존 open의 반댓값
 
+    };
+
+    const todoChangeHandler = e => {
+        // console.log(e.target.value);
+        setTodoText(e.target.value);
+    };
+
+    const submitHandler = e => {
+        e.preventDefault(); {/* 보냈을때 페이지가 다시로딩되는걸 막음 */}
+        onAdd(todoText);
+
+        // 폼이 제출되면 입력창 비우기
+        setTodoText('');
     }
 
     return (
         <>
             {open && (<div className='form-wrapper'> {/* 둘다 true이면 open 기본값이 false */}
-                <form className='insert-form'>
+                <form className='insert-form' onSubmit={submitHandler}>
                     <input
                         type='text'
                         placeholder='할 일을 입력 후, 엔터를 누르세요!'
+                        onChange={todoChangeHandler}
                     />
                 </form>
             </div>)}
